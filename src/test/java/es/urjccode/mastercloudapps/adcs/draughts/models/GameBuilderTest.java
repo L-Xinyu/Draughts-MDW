4a63f3d787b9bb080ad68063d3c193c5483e8b2d
XinyuLi <911410598@qq.com>
2020-04-26 16:39:03 +0200

    Modify GameBuilder&Test and Delete character isUpperCase

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameBuilderTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameBuilderTest.java
@@ -21,7 +21,6 @@ public class GameBuilderTest {
         this.gameBuilder.rows(
             "        ",
             "        ",
- //           "        ",
             "        ",
             "        ",
             "        ",
